dd01bac023fde4d1ec2eaaeaf531711483fed8ae
OpAue <kkt23917@naver.com>
2026-07-07 18:13:25 +0900

    Create_Project

---
+++ BE/Myeongrang-hoe/src/main/java/com/example/myeongranghoe/MyeongrangHoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.myeongranghoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeongrangHoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyeongrangHoeApplication.class, args);
+    }
+
+}
---
+++ BE/Myeongrang-hoe/src/test/java/com/example/myeongranghoe/MyeongrangHoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myeongranghoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeongrangHoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
